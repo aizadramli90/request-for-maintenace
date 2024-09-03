25b13d3f782dcb024c6c0eaa30269b519f5adcb9
aizadramli90 <117175977+aizadramli90@users.noreply.github.com>
2024-09-03 19:42:44 +0800

    redirection

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,22 @@
 //import all the required modules
 import 'dotenv/config'
 import express from "express";
-import healthController from "./controller/health.js";
-import { databaseInit } from "./database/connection.js";
-import createUser from "./controller/user.controller/create.js";
-import { readAllUsers, readUserById } from "./controller/user.controller/read.js";
-// import updateUser from "./controller/user.controller/update.js";
-import deleteUser from "./controller/user.controller/delete.js";
-import createRequest from "./controller/request.controller/create.js";
-import deleteRequest from "./controller/request.controller/delete.js";
-import listAllRequestForSpecificId from "./controller/request.controller/read.js";
-import createToken from "./controller/auth.js";
-import isAuth from "./middleware/isAuth.js";
-/* import { uploadImage } from "./controller/upload.js";
-import upload from "./middleware/multerUpload.js"; */
-import path from "path";
-import { fileURLToPath } from 'url';
-import cors from "cors";
+// import healthController from "./controller/health.js";
+// import { databaseInit } from "./database/connection.js";
+// import createUser from "./controller/user.controller/create.js";
+// import { readAllUsers, readUserById } from "./controller/user.controller/read.js";
+// // import updateUser from "./controller/user.controller/update.js";
+// import deleteUser from "./controller/user.controller/delete.js";
+// import createRequest from "./controller/request.controller/create.js";
+// import deleteRequest from "./controller/request.controller/delete.js";
+// import listAllRequestForSpecificId from "./controller/request.controller/read.js";
+// import createToken from "./controller/auth.js";
+// import isAuth from "./middleware/isAuth.js";
+// /* import { uploadImage } from "./controller/upload.js";
+// import upload from "./middleware/multerUpload.js"; */
+// import path from "path";
+// import { fileURLToPath } from 'url';
+// import cors from "cors";
 
 
 
@@ -34,67 +34,66 @@ const PORT = process.env.PORT;
 
 //redirect to new website
 
+// redirect to new website https://reforms-pern-stack.onrender.com/
 app.all('*', (req, res) => {
   res.redirect(301, 'https://reforms-pern-stack.onrender.com/');
 });
 
-app.listen(80, () => {
-  console.log('Redirect server listening on port 80');
-});
+app.listen(80, () => console.log('Redirect server listening on port 80'));
 
 
 
-//parsing
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+// //parsing
+// app.use(express.json());
+// app.use(express.urlencoded({extended: true}));
 
-//serve static files from the ../frontend/public folder
-//app.use(express.static(path.join(__dirname, '../frontend/src/reforms_web_app_react/public')));
-app.use(express.static(path.join(__dirname, '../frontend/src/reforms_web_app_react/dist')));
-
-
-
-//database connection and initialization
-databaseInit();
-
-// cors
-app.use(cors(
-    {
-        origin: process.env.FRONTEND_URL,
-        credentials: true
-    }
-));
+// //serve static files from the ../frontend/public folder
+// //app.use(express.static(path.join(__dirname, '../frontend/src/reforms_web_app_react/public')));
+// app.use(express.static(path.join(__dirname, '../frontend/src/reforms_web_app_react/dist')));
 
 
 
-//routes
-app.get("/", healthController.get);
-app.post("/", healthController.post);
-app.get("/admin/users", isAuth, readAllUsers);
-app.delete("/users/:id", deleteUser)
+// //database connection and initialization
+// databaseInit();
 
-app.post("/register", createUser);
-app.post("/login", createToken)
+// // cors
+// app.use(cors(
+//     {
+//         origin: process.env.FRONTEND_URL,
+//         credentials: true
+//     }
+// ));
 
-//route to handle requests
-app.post("/requests", isAuth, createRequest);
-app.get("/requests", isAuth, listAllRequestForSpecificId);
 
-//delete request
-app.delete("/request/:id", isAuth, deleteRequest);
 
-//upload image - TO BE ADDED SOON
-/* app.post("/upload", isAuth, upload.single('image'), uploadImage);
- */
-//route to handle 404 and show page 404.html
-app.use((req, res) => {
-    /* res.status(404).json({message: "Route not found"}); */
-    res.status(404).sendFile(path.join(__dirname, '../frontend/public', '404.html'));
+// //routes
+// app.get("/", healthController.get);
+// app.post("/", healthController.post);
+// app.get("/admin/users", isAuth, readAllUsers);
+// app.delete("/users/:id", deleteUser)
+
+// app.post("/register", createUser);
+// app.post("/login", createToken)
+
+// //route to handle requests
+// app.post("/requests", isAuth, createRequest);
+// app.get("/requests", isAuth, listAllRequestForSpecificId);
+
+// //delete request
+// app.delete("/request/:id", isAuth, deleteRequest);
+
+// //upload image - TO BE ADDED SOON
+// /* app.post("/upload", isAuth, upload.single('image'), uploadImage);
+//  */
+// //route to handle 404 and show page 404.html
+// app.use((req, res) => {
+//     /* res.status(404).json({message: "Route not found"}); */
+//     res.status(404).sendFile(path.join(__dirname, '../frontend/public', '404.html'));
     
-})
+// })
 
 
-/* app.get('/', (req, res) => res.send('Hello World!')) */
+// /* app.get('/', (req, res) => res.send('Hello World!')) */
 
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}!`))
